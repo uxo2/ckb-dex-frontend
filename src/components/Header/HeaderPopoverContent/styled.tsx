4fcf48fc60e43a01299bf3a1b6b0384ac6bd800f
UXO2 <30302637+uxo2@users.noreply.github.com>
2020-10-13 09:09:09 +0800

    Revert "feat： Add language conversion: chinese and english"

--- src/components/Header/HeaderPopoverContent/styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const PopoverBox = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 300px;
-  height: 200px;
-`
